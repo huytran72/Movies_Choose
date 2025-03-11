687bf420e377d68cff8fa81f0e1233ba7bb26fe0
Huy Tran <baohuy82.th@gmail.com>
2025-03-11 09:14:43 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -68,13 +68,7 @@ function NavBar() {
         <span role="img">🍿</span>
         <h1>MoviesChoose</h1>
       </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+
       <p className="num-results">
         Found <strong>X</strong> results
       </p>
