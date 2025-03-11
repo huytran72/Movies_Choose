722d012e64453a3196f7f6568f2a3760a03a37f7
Huy Tran <baohuy82.th@gmail.com>
2025-03-11 09:15:51 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -66,7 +66,7 @@ function NavBar() {
         <span role="img">🍿</span>
         <h1>MoviesChoose</h1>
       </div>
-
+      <Search />
       <p className="num-results">
         Found <strong>X</strong> results
       </p>
