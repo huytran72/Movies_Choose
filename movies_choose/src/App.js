6a35d104d6ed7fb39265d90cc54491dd0e7bd794
Huy Tran <baohuy82.th@gmail.com>
2025-03-12 23:31:52 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -172,4 +172,31 @@ function Main() {
   );
 }
 
-function ListBox() {}
+function ListBox() {
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+      {isOpen1 && (
+        <ul className="list">
+          {movies?.map((movie) => (
+            <li key={movie.imdbID}>
+              <img src={movie.Poster} alt={`${movie.Title} poster`} />
+              <h3>{movie.Title}</h3>
+              <div>
+                <p>
+                  <span>🗓</span>
+                  <span>{movie.Year}</span>
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
