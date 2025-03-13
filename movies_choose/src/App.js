c75508e9bf2857a08ff0e903fab7f6bf9072f4ff
Huy Tran <baohuy82.th@gmail.com>
2025-03-12 23:44:41 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -137,7 +137,20 @@ function MovieList() {
   );
 }
 
-function Movie() {}
+function Movie() {
+  return (
+    <li key={movie.imdbID}>
+          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>🗓</span>
+              <span>{movie.Year}</span>
+            </p>
+          </div>
+        </li>
+  )
+}
 
 function WatchedBox() {
   const [watched, setWatched] = useState(tempWatchedData);
