8626c6717d64b007f29b4f2d6e91e03943bb5860
Huy Tran <baohuy82.th@gmail.com>
2025-03-13 23:46:58 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -170,28 +170,6 @@ function WatchedBox() {
       </button>
       {isOpen2 && (
         <>
-          <div className="summary">
-            <h2>Movies you watched</h2>
-            <div>
-              <p>
-                <span>#️⃣</span>
-                <span>{watched.length} movies</span>
-              </p>
-              <p>
-                <span>⭐️</span>
-                <span>{avgImdbRating}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{avgUserRating}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{avgRuntime} min</span>
-              </p>
-            </div>
-          </div>
-
           <ul className="list">
             {watched.map((movie) => (
               <li key={movie.imdbID}>
