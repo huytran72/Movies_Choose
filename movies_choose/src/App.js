d72c391a2153af5d7994087f43519efe09a98102
Huy Tran <baohuy82.th@gmail.com>
2025-03-14 23:29:34 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -97,7 +97,7 @@ function Search() {
 function NumResults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
