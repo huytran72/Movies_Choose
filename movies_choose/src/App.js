25727a700437b6d636e486b0dfb8d38ba0e28464
Huy Tran <baohuy82.th@gmail.com>
2025-03-15 23:36:34 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -55,7 +55,7 @@ export default function App() {
 
   return (
     <div>
-      <NavBar movies={movies} />
+      <NavBar movies={movies} ><Logo />
       <Main movies={movies} />;
     </div>
   );
