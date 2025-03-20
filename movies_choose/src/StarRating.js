a6fb8fbf5d4168548c3e820406bc0f5f16dfe788
Huy Tran <baohuy82.th@gmail.com>
2025-03-19 23:19:26 -0700

    Update StarRating.js

--- movies_choose/src/StarRating.js
+++ movies_choose/src/StarRating.js
@@ -2,7 +2,9 @@ export default function StarRating({ rating, setRating }) {
   return (
     <div>
       <div>
-        {Array.from({ length: 5 }, (_, i) => <span>S{i+1}</span>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
       </div>
     </div>
   );
