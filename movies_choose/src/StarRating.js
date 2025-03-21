c987e0fb82833ff6aa9b76c49dc7d9559d0ee120
Huy Tran <baohuy82.th@gmail.com>
2025-03-20 23:38:05 -0700

    Update StarRating.js

--- movies_choose/src/StarRating.js
+++ movies_choose/src/StarRating.js
@@ -1,13 +1,12 @@
 export default function StarRating({ rating, setRating }) {
-    const containerStyle = {
-        display: "flex",
-        alignItems: "center",
-        gap: "16px",
-    };
+  const containerStyle = {
+    display: "flex",
+    alignItems: "center",
+    gap: "16px",
+  };
 
-
-    return (
-    <div style={}>
+  return (
+    <div style={containerStyle}>
       <div>
         {Array.from({ length: 5 }, (_, i) => (
           <span>S{i + 1}</span>
