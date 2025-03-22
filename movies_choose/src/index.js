3b29099ea00cde7e84f0a32b020a94a9237fa7e1
Huy Tran <baohuy82.th@gmail.com>
2025-03-21 23:46:00 -0700

    Update index.js

--- movies_choose/src/index.js
+++ movies_choose/src/index.js
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
