d09cd1915368261a9acc1fcf5dd073c9f1feeb7a
Huy Tran <baohuy82.th@gmail.com>
2025-03-23 23:53:49 -0700

    Update StarRating.js

--- movies_choose/src/StarRating.js
+++ movies_choose/src/StarRating.js
@@ -39,9 +39,9 @@ const starStyle = {
   cursor: "pointer",
 };
 
-function Star({ onClick }) {
+function Star({ onRate }) {
   return (
-    <span role="'button" style={starStyle} onClick={onClick}>
+    <span role="'button" style={starStyle} onClick={onRate}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
