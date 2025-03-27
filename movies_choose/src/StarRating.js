01ec6e35563ed4bcc0c8aee6faa253cdbacd91b8
Huy Tran <baohuy82.th@gmail.com>
2025-03-26 23:33:44 -0700

    Update StarRating.js

--- movies_choose/src/StarRating.js
+++ movies_choose/src/StarRating.js
@@ -16,7 +16,11 @@ const textStyle = {
   margin: "0",
 };
 
-export default function StarRating({ maxRating = 5, color, size }) {
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+}) {
   const [rating, setRating] = useState(0);
   const [temRating, setTempRating] = useState(0);
 
