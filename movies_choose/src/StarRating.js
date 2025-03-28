92ce4a669b3a3e5d05410d5d20517715f07dd713
Huy Tran <baohuy82.th@gmail.com>
2025-03-27 23:28:56 -0700

    Update StarRating.js

--- movies_choose/src/StarRating.js
+++ movies_choose/src/StarRating.js
@@ -81,7 +81,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
