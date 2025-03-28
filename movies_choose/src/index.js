c8e8a440dccb7c9896b780dee00299c8f2a8667d
Huy Tran <baohuy82.th@gmail.com>
2025-03-27 23:29:30 -0700

    Update index.js

--- movies_choose/src/index.js
+++ movies_choose/src/index.js
@@ -10,6 +10,6 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
-    <StarRating size={24} />
+    <StarRating size={24} color="red" />
   </React.StrictMode>
 );
