6b3f09fd1e1bcbd85b7219111d99b17290230b23
Huy Tran <baohuy82.th@gmail.com>
2025-03-29 23:26:17 -0700

    Update index.js

--- movies_choose/src/index.js
+++ movies_choose/src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from "react-dom/client";
 
 import StarRating from "./StarRating";
 
+function Test() {}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
