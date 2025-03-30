223f714b678e49e76de8a2d3d9d74c57959ee08e
Huy Tran <baohuy82.th@gmail.com>
2025-03-29 23:26:55 -0700

    Update index.js

--- movies_choose/src/index.js
+++ movies_choose/src/index.js
@@ -18,5 +18,7 @@ root.render(
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
+
+    <Test />
   </React.StrictMode>
 );
