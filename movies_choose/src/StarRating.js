d3a495192ac944815f62ecc0436491a3c0246436
Huy Tran <baohuy82.th@gmail.com>
2025-03-30 23:45:12 -0700

    Update StarRating.js

--- movies_choose/src/StarRating.js
+++ movies_choose/src/StarRating.js
@@ -16,6 +16,7 @@ StarRating.propTypes = {
   color: PropTypes.string,
   size: PropTypes.number,
   className: PropTypes.string,
+  messages: PropTypes.array,
 };
 
 export default function StarRating({
