4cb86d2c2a9cd491d9483398af57dbed48600266
Huy Tran <baohuy82.th@gmail.com>
2025-04-06 23:54:53 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -56,6 +56,7 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const query = "interstellar";
 
   useEffect(function () {
