7ae17af6cbb43a9eb98fc943e264a4c8eceabc5f
Huy Tran <baohuy82.th@gmail.com>
2025-04-08 23:40:02 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -73,7 +73,7 @@ export default function App() {
         if (res.Response === "False") throw new Error("Movie not found");
 
         setMovies(data.Search);
-        // console.log(movies);
+        console.log(movies);
         // console.log(data.Search);
       } catch (err) {
         console.error(err.message);
