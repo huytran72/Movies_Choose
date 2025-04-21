a4f49c95175d1842747b5d91efc6cb8da501b753
Huy Tran <baohuy82.th@gmail.com>
2025-04-20 23:37:51 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -377,8 +377,8 @@ function WatchedMoviesList({ watched }) {
 function WatchedMovie({ movie }) {
   return (
     <li key={movie.imdbID}>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
+      <img src={movie.Poster} alt={`${movie.title} poster`} />
+      <h3>{movie.title}</h3>
       <div>
         <p>
           <span>⭐️</span>
