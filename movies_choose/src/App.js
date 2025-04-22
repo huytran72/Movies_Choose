5e0ba736d02c491b48f068d298cc860a72612d62
Huy Tran <baohuy82.th@gmail.com>
2025-04-21 23:49:32 -0700

    Update App.js

--- movies_choose/src/App.js
+++ movies_choose/src/App.js
@@ -280,7 +280,7 @@ function Movie({ movie, onSelectMovie }) {
 function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
-  const [userRating, setUserRating] = useState(0);
+  const [userRating, setUserRating] = useState('');
 
   const {
     Title: title,
